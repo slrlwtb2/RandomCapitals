6d1f1c7e4088f473c0a51ce327dfa46105ca38f1
slrlw <slrlw@RICHY>
2024-03-21 20:26:56 +0700

    Add project files.

---
+++ RandomCapitals/Controllers/RandomContinentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RandomContinent.Service;
+
+namespace RandomContinent.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RandomContinentController : ControllerBase
+    {
+        private readonly RandomContinentService _randomContinent;
+        public RandomContinentController(RandomContinentService randomContinent)
+        {
+             _randomContinent = randomContinent;
+        }
+        [HttpGet]
+        public IActionResult RandomContinent()
+        {
+            var coordinates = _randomContinent.GenerateRandomCoordinates();
+            return Ok(new { Player1Coordinates = coordinates[0], Player2Coordinates = coordinates[1] });
+        }
+    }
+}
---
+++ RandomCapitals/Model/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace RandomContinent.Model
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int[] Continent { get; set; }
+        public int Money { get; set; }
+    }
+}
